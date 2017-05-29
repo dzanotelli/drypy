31de1a20e2880f60f68c8d0093a28c33cb209bb9
Daniele Zanotelli <dazano@gmail.com>
2017-05-29 11:33:49 +0200

    added some docstring to general functions

--- drypy/__init__.py
+++ drypy/__init__.py
@@ -4,12 +4,18 @@ from . import version
 logger = logging.getLogger(__name__)
 
 def get_version():
+    """
+    Return the current drypy version.
+    """
     return version._version
 
 # dryrun switcher
 _dryrun = False
 
 def get_status():
+    """
+    Return True if the dryrun system is active.
+    """
     if _dryrun not in (True, False):
         msg = "Don't manually change the _dryrun flag! Use 'set_dryrun' "
         msg += "instead. Dryrun set to False."
@@ -18,12 +24,18 @@ def get_status():
     return _dryrun
 
 def set_dryrun(value):
+    """
+    Set the dryrun on or off.
+    """
     global _dryrun
     if type(value) is not bool:
         raise TypeError("Boolean required")
     _dryrun = value
 
 def toggle_dryrun():
+    """
+    Toggle current dryrun status.
+    """
     if get_status() is True:
         set_dryrun(False)
     else:
