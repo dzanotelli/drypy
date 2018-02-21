cbc4d90f3ce406d6af2267a3d87d6f31e4564a0b
Daniele Zanotelli <dazano@gmail.com>
2018-02-21 16:50:44 +0100

    fixed setup file: removed useless stuff, added url, fixed classifiers; refs #7

--- setup.py
+++ setup.py
@@ -18,53 +18,34 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 # read requirements.txt file
 required_file = parse_requirements("requirements.txt", session=PipSession())
-required_all = [str(ir.req) for ir in required_file]
-
-required_dev = ['ipython']
-required = []
-
-# add to required all the requirements.txt entries but the ones in reqreuied_dev
-required.extend([entry for entry in required_all if entry not in required_dev])
-
+required = [str(ir.req) for ir in required_file]
 
 setup(
     name="drypy",
     version=get_version(),
     description="Python utilities to perform dryrun.",
     long_description=long_description,
+    url="https://github.com/dzanotelli/drypy",
     author="Daniele Zanotelli",
     author_email="dazano@gmail.com",
     license="MIT",
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
-
-        # Indicate who your project is intended for
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-
-        # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
-
-        # Should work on all posix systems
         "Operating System :: POSIX",
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
     ],
 
     keywords="dryrun",
-    packages=find_packages(exclude="tests"),
+    packages=find_packages(exclude=["docs", "tests"]),
     install_requires=required,
 
     extras_require={
-        'dev': required_dev,
+        'dev': [],
     },
 
     package_data={},
