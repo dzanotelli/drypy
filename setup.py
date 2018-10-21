aca57092d08a91839b695151ad52a7129ded0e49
Daniele Zanotelli <dazano@gmail.com>
2018-10-21 12:54:33 +0200

    updated setup.py file

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 """Dryrun for Python
 
-(C) 2017 Daniele Zanotelli
+(C) 2017-2018 Daniele Zanotelli
+
 """
 
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-try:  # for pip >= 10
-    from pip._internal.download import PipSession
-except ImportError:  # for pip <= 9.0.3
-    from pip.download import PipSession
 from drypy import get_version
-
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
+# get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 # read requirements.txt file
-required_file = parse_requirements("requirements.txt", session=PipSession())
-required = [str(ir.req) for ir in required_file]
+with open(path.join(here, 'requirements.txt'), 'r') as f:
+        required_all = f.read().strip().split('\n')
+
+required_dev = ['ipython', 'sphinx_rtd_theme']
+required = [item for item in required_all if item not in required_dev]
 
 setup(
     name="drypy",
@@ -39,7 +34,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
